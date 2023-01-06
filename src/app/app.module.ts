875ef46e6765aa88a1b63ab9756f4ecb8b31cdc9
GabyDs <gabydsch2001@gmail.com>
2023-01-06 17:08:02 -0300

    add experience and education components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { AboutComponent } from './components/about/about.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
     AboutComponent,
+    ExperienceComponent,
+    EducationComponent,
   ],
   imports: [
     BrowserModule,
