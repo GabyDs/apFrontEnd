a799805106edad8c190fbafba81d7d4f49eca2c6
GabyDs <gabydsch2001@gmail.com>
2023-01-09 22:11:57 -0300

    configured login.ts

--- src/app/model/jwt-dto.ts
+++ src/app/model/jwt-dto.ts
@@ -1,6 +1,6 @@
 export class JwtDto {
-    token!: String;
-    type!: String;
-    userName!: String;
-    authorities!: String[];
+    token!: string;
+    type!: string;
+    userName!: string;
+    authorities!: string[];
 }
